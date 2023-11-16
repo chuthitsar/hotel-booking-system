0f92feb09ea17825d54eb12c298e970cc2ebeccf
Chu Thit Sar <chuthitsar12@gmail.com>
2023-11-16 11:02:44 +0630

    refactor html file name

--- src/main/java/com/nexcode/hbs/model/exception/GlobalExceptionHandler.java
+++ src/main/java/com/nexcode/hbs/model/exception/GlobalExceptionHandler.java
@@ -1,8 +1,15 @@
 package com.nexcode.hbs.model.exception;
 
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -10,6 +17,20 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(RuntimeException.class)
     public ResponseEntity<?> handleRuntimeException(RuntimeException ex) {
         throw ex;
+    }
+	
+	@ExceptionHandler(InvalidCredentialsException.class)
+    public final ResponseEntity<Object> handleInvalidCredentialsException(InvalidCredentialsException ex, WebRequest request) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.UNAUTHORIZED.value());
+        body.put("error", "Unauthorized");
+        body.put("message", ex.getMessage());
+        body.put("path", ((ServletWebRequest)request).getRequest().getRequestURI());
+
+        return new ResponseEntity<>(body, HttpStatus.UNAUTHORIZED);
+        
+//        throw new InvalidCredentialsException("Incorrect");
     }
 	
 }
--- src/main/java/com/nexcode/hbs/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/nexcode/hbs/security/JwtAuthenticationEntryPoint.java
@@ -10,6 +10,8 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
+import com.nexcode.hbs.model.exception.InvalidCredentialsException;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Component
@@ -21,6 +23,8 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 			AuthenticationException authException) throws IOException, ServletException {
 
 		log.error("Unauthorized error: {}", authException.getMessage());
+		System.out.println("entry point");
+		//throw new InvalidCredentialsException(authException.getMessage());
 		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error -> Unauthorized->" + authException.getMessage());
 	}
 
